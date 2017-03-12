0a99f02afd47f6f5752d3494c036f2957241c101
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-03-12 13:27:15 -0300

    Try with coverage.sh

---
+++ coverage.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+find . -name "*.gcno" | while read line ; do
+  gcda=`echo $line | sed 's/\.gcno$/.gcda/'` ;
+  if [ \! -e $gcda ]  ; then
+    touch $gcda && echo created empty $gcda;
+  fi ;
+done
+
+lcov --directory ../ --capture --output-file coverage.info
+lcov --remove coverage.info '/test/*' 'build/ext/*' '/usr/*' --output-file coverage.info
+lcov --list coverage.info
+
+coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info
