7841a1f81fab6d0c9f7cc34c2d3598fffb8fa6c8
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-03-19 16:18:39 -0300

    Try another coveralls alternative

--- coverage.sh
+++ coverage.sh
@@ -7,8 +7,8 @@
 #  fi ;
 #done
 
-lcov --directory ../ --capture --output-file coverage.info
-lcov --remove coverage.info '/test/*' 'build/ext/*' '/usr/*' --output-file coverage.info
-lcov --list coverage.info
+#lcov --directory ../ --capture --output-file coverage.info
+#lcov --remove coverage.info '/test/*' 'build/ext/*' '/usr/*' --output-file coverage.info
+#lcov --list coverage.info
 
-coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info
+#coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info
