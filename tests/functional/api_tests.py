8ac070e77864f0e1946f90735c1c6f5d39a55e0e
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-03-26 23:14:20 -0300

    Add simple functional test

---
+++ tests/functional/api_tests.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import requests
+import unittest
+import json
+from subprocess import call
+from multiprocessing import Process
+import os
+import signal
+
+
+class testTallerifyAppServer(unittest.TestCase):
+    def testPing(self):
+        response = requests.get('http://localhost:8080/ping')
+        self.assertEqual(200, response.status_code)
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(testTallerifyAppServer)
+
+
+def startAppServer():
+    call(["./build/fiuba_taller2_tallerify_app_server"])
+
+
+def killAppServer():
+    for line in os.popen("ps ax | grep fiuba_taller2_tallerify_app_server | grep -v grep"):
+        fields = line.split()
+        pid = fields[0]
+    os.kill(int(pid), signal.SIGKILL)
+
+
+def runTests():
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+t1 = Process(target=startAppServer, args=())
+t2 = Process(target=runTests, args=())
+t1.start()
+t2.start()
+t2.join()
+killAppServer()
