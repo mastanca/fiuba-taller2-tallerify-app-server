86b170545d8f52fe120435e2ef3ac74620279215
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-03-30 01:10:13 -0300

    Add 500 test

---
+++ tests/unit/ControllerTest.h
@@ -0,0 +1,30 @@
+
+
+#ifndef FIUBA_TALLER2_TALLERIFY_APP_SERVER_CONTROLLERTEST_H
+#define FIUBA_TALLER2_TALLERIFY_APP_SERVER_CONTROLLERTEST_H
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../../src/api/controllers/Controller.h"
+
+class ControllerTest : public testing::Test {
+public:
+    ControllerTest() {}
+
+    void TestBody() {}
+
+    void testInternalServerError();
+
+};
+
+TEST_F(ControllerTest, testInternalServerError
+) {
+ControllerTest tester;
+tester.
+
+testInternalServerError();
+
+}
+
+
+#endif //FIUBA_TALLER2_TALLERIFY_APP_SERVER_CONTROLLERTEST_H
