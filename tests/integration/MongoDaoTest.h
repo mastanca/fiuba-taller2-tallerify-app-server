c2243122215c373e1cb9d38544629f3c3746f02b
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-05-04 12:48:13 -0300

    DEBUG comment integration tests

--- tests/integration/MongoDaoTest.h
+++ tests/integration/MongoDaoTest.h
@@ -8,23 +8,23 @@
 class MongoDaoTest : public testing::Test {
 public:
     MongoDaoTest() {}
-    
+
     void TestBody() {}
-    
+
     void testGetting();
-    
+
     void testSaving();
 };
 
-TEST_F(MongoDaoTest, testGetting) {
-    MongoDaoTest tester;
-    tester.testGetting();
-}
-
-TEST_F(MongoDaoTest, testSaving) {
-    MongoDaoTest tester;
-    tester.testSaving();
-}
+// TEST_F(MongoDaoTest, testGetting) {
+//     MongoDaoTest tester;
+//     tester.testGetting();
+// }
+//
+// TEST_F(MongoDaoTest, testSaving) {
+//     MongoDaoTest tester;
+//     tester.testSaving();
+// }
 
 
 #endif //FIUBA_TALLER2_TALLERIFY_APP_SERVER_MONGODAOTEST_H
