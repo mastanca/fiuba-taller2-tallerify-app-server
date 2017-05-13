c7cfb2b128269d67ed81f9b25848f5624aa72b13
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-05-13 15:01:26 -0300

    Update docs

--- doc/html/search/all_7.js
+++ doc/html/search/all_7.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['request',['Request',['../class_request.html',1,'Request'],['../class_request.html#a135fd74afd31ea5b1934456cd17dbfba',1,'Request::Request()']]],
+  ['request',['Request',['../class_request.html',1,'Request'],['../class_request.html#a135fd74afd31ea5b1934456cd17dbfba',1,'Request::Request(mg_connection *connection, http_message *httpMessage)'],['../class_request.html#a18eac53d1c3271949bc437a9fc59eb8d',1,'Request::Request(mg_connection *connection, int event, void *eventData)']]],
   ['request_2eh',['Request.h',['../_request_8h.html',1,'']]],
   ['response',['Response',['../class_response.html',1,'Response'],['../class_response.html#aa9b6e391d77fce8daca8bac13730ec8e',1,'Response::Response()']]],
-  ['response_2eh',['Response.h',['../_response_8h.html',1,'']]]
+  ['response_2eh',['Response.h',['../_response_8h.html',1,'']]],
+  ['returntype',['returnType',['../structreturn_type.html',1,'']]]
 ];
--- doc/html/search/classes_4.js
+++ doc/html/search/classes_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['request',['Request',['../class_request.html',1,'']]],
-  ['response',['Response',['../class_response.html',1,'']]]
+  ['response',['Response',['../class_response.html',1,'']]],
+  ['returntype',['returnType',['../structreturn_type.html',1,'']]]
 ];
