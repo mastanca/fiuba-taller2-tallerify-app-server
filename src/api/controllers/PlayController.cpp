8dd3c3d13b9d356793ae228eb0e64726538c77ab
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-06-01 00:17:18 -0300

    Set filelocation for docker and ide

--- src/api/controllers/PlayController.cpp
+++ src/api/controllers/PlayController.cpp
@@ -31,7 +31,7 @@ Response *PlayController::process(Request &request) {
 }
 
 void PlayController::get(Request &request, JSONResponse &response) {
-    std::string fileLocation = "./music/" + request.getElementIdString(); // Compose and console run location
+    std::string fileLocation = "../music/" + request.getElementIdString(); // Compose and ide location
     mg_http_serve_file(request.getConnection(), request.getHttpMessage(), fileLocation.c_str(),
                        mg_mk_str("audio/mpeg3"), mg_mk_str(""));
 }
